76056264331c3f00f7195033606722e16772b2d0
Iggy <igoririanto@rocketmail.com>
2018-06-19 21:31:24 -0500

    first commit

--- lib/elixir_web.ex
+++ lib/elixir_web.ex
@@ -1,18 +1,2 @@
 defmodule ElixirWeb do
-  @moduledoc """
-  Documentation for ElixirWeb.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirWeb.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
